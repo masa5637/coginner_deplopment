21d3812ed8f89930bb83d3b6fe9b39ff3ecc1043
masa <masada59d@gmail.com>
2025-09-29 23:41:36 +0900

    aaa

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,18 +1,19 @@
 class WorksController < ApplicationController
+  before_action :set_work, only: [:show, :edit, :update, :destroy]
+
   def index
     @q = Work.ransack(params[:q])
 
     if params[:q].present?
-    # 検索条件がある場合はransackの結果を返す
+      # 検索条件がある場合はransackの結果を返す
       @works = @q.result(distinct: true).includes(:user, :tags)
     else
-    # 検索条件がない場合は全作品を返す
+      # 検索条件がない場合は全作品を返す
       @works = Work.includes(:user, :tags).all
     end
   end
 
   def show
-    @work = Work.find(params[:id])
     @show_share_button = true
   end
 
@@ -38,11 +39,39 @@ class WorksController < ApplicationController
     end
   end
 
+  def edit
+    # @work は before_action で取得済み
+  end
+
+  def update
+    if @work.update(work_params.except(:images))
+      # 追加の画像があればattach
+      if work_params[:images]
+        work_params[:images].each do |image|
+          @work.images.attach(image)
+        end
+      end
+      redirect_to work_path(@work), notice: "作品を更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
+  end
+
+  def destroy
+    @work.destroy
+    redirect_to works_path, notice: "投稿を削除しました"
+  end
+
   private
+
+  def set_work
+    @work = Work.find(params[:id])
+  end
 
   def work_params
     permitted = params.require(:work).permit(:title, :description, images: [], tag_ids: [])
-    
+
     # images 配列の中の空文字を削除
     if permitted[:images]
       permitted[:images].reject!(&:blank?)
