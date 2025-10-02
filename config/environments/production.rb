7b21d40c35cd3143e856b6d3820c6a666a5f74ae
masa <masada59d@gmail.com>
2025-10-02 12:55:50 +0900

    aaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << "coginner-np8s.onrender.com"
+  config.hosts << /.*\.onrender\.com/
 end
