271adf671ff770ff26d1a83736e52fb0cc2fbfe0
masa <masada59d@gmail.com>
2025-10-02 17:16:05 +0900

    aaa

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
-// Entry point for the build script in your package.json
+// Entry point for JS build
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
-// import "bootstrap-icons/font/bootstrap-icons.css";  ← コメントアウト
+
+// bootstrap の CSS は SCSS 側で読み込むので JS 側では不要
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap-icons/font/bootstrap-icons.css"
