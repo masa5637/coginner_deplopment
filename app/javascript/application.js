c8f9d1c057f5888d37a9741ad845b292b5fe53d0
masa <masada59d@gmail.com>
2025-10-02 16:13:59 +0900

    aaa

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
